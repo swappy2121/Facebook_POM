05d393a81cb542e99cb4d226699481692b6d9c0b
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-09 01:27:15 -0700

    Update BaseClass.java

--- src/main/java/com/facebook/qa/baseclass/BaseClass.java
+++ src/main/java/com/facebook/qa/baseclass/BaseClass.java
@@ -7,6 +7,21 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
     public static WebDriver driver;
+    public static Properties prop;
+
+    public void testBase(){
+    try {
+        prop = new Properties();
+        FileInputStream ip = new FileInputStream(System.getProperty("C:\\Users\\HP\\IdeaProjects\\Facebook_POM\\src\\main\\java\\com\\facebook\\qa\\config\\Config_Data.properties"));
+        try {
+            prop.load(ip);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+    }
 
     public void setProperties(){
         System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
