23c4bfeefcefb9a0480c3eb8b81cd0e8f59f435c
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-12 00:59:42 -0700

    Update SignUpPageTestCase.java

--- src/test/java/com/facebook/qa/testcasses/SignUpPageTestCase.java
+++ src/test/java/com/facebook/qa/testcasses/SignUpPageTestCase.java
@@ -4,8 +4,10 @@ import com.facebook.qa.pages.SignUpPage;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(com.qa.extentreportlistener.TestNG_Listener.class)
 public class SignUpPageTestCase extends SignUpPage{
 
     public SignUpPageTestCase(){
@@ -24,52 +26,52 @@ public class SignUpPageTestCase extends SignUpPage{
     @Test(priority = 2)
     public void verifyFirstNameOfSignUpPage(){
         String Name1 = verifyFirstNameField();
-        Assert.assertEquals(Name1,"firstname");
+        Assert.assertEquals(Name1,"Swapnil");
     }
     @Test(priority = 3)
     public void verifyLastNameOfSignUpPage(){
         String Name2 = verifyLastNameField();
-        Assert.assertEquals(Name2,"lastname");
+        Assert.assertEquals(Name2,"Pawar");
     }
     @Test(priority = 4)
     public void verifyEmailFieldOfSignUpPage(){
         String email = verifyEmailTextField();
-        Assert.assertEquals(email,"reg_email__");
+        Assert.assertEquals(email,"swapnilPawar25@gmail.com");
     }
     @Test(priority = 5)
     public void verifyConfirmEmailFieldOfSignUpPage(){
         String confEmail = verifyConfirmEmailTextField();
-        Assert.assertEquals(confEmail,"reg_email_confirmation__");
+        Assert.assertEquals(confEmail,"swapnilPawar25@gmail.com");
     }
     @Test(priority = 6)
     public void verifypassFieldOfSignUpPage(){
         String pass = verifyPasswordTextField();
-        Assert.assertEquals(pass,"reg_passwd__");
+        Assert.assertEquals(pass,"Swappy@2524");
     }
     @Test(priority = 7)
     public void verifyBirthDayFieldOfSignUpPage(){
         String birth_Day = verifyBirthDayField();
-        Assert.assertEquals(birth_Day,"birthday_day");
+        Assert.assertEquals(birth_Day,"5");
     }
     @Test(priority = 8)
     public void verifyBirthMonthFieldOfSignUpPage(){
         String birth_Month = verifyBirthmonthField();
-        Assert.assertEquals(birth_Month,"birthday_month");
+        Assert.assertEquals(birth_Month,"9");
     }
     @Test(priority = 9)
     public void verifyBirthYearFieldOfSignUpPage(){
         String birth_Year = verifyBirthYearField();
-        Assert.assertEquals(birth_Year,"birthday_year");
+        Assert.assertEquals(birth_Year,"1999");
     }
     @Test(priority = 10)
     public void verifyGenderFieldOfSignUpPage(){
         String gender = verifyGenderField();
-        Assert.assertEquals(gender,"sex");
+        Assert.assertEquals(gender,"2");
     }
     @Test(priority = 11)
     public void verifySubmitbtnOfSignUpPage(){
-        String subBtn = verifySignUpBtnField();
-        Assert.assertEquals(subBtn,"websubmit");
+        boolean subBtn = verifySignUpBtnField();
+        Assert.assertTrue(subBtn);
     }
     @AfterTest
     public void closeBrowsers(){
