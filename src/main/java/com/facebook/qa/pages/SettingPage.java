bb422d6c185518053b2691ba29c3954b37e9d802
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-12 00:54:53 -0700

    Update SettingPage.java

--- src/main/java/com/facebook/qa/pages/SettingPage.java
+++ src/main/java/com/facebook/qa/pages/SettingPage.java
@@ -18,45 +18,45 @@ public class SettingPage extends BaseClass {
     public static WebElement businessIntegration;
 
 
-    public String verifyGeneralField(){
+    public boolean verifyGeneralField(){
         general = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[1]"));
-        return general.getTagName();
+        return general.isEnabled();
     }
-    public String verifySecurityAndPrivacyField(){
+    public boolean verifySecurityAndPrivacyField(){
         securityAndPrivacy = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[2]"));
-        return securityAndPrivacy.getAttribute("class");
+        return securityAndPrivacy.isEnabled();
     }
-    public String verifyYourFacebookInfoField(){
+    public boolean verifyYourFacebookInfoField(){
         yourFacebookInfo = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[3]"));
-        return yourFacebookInfo.getAttribute("class");
+        return yourFacebookInfo.isEnabled();
     }
-    public String verifyPrivacyField(){
+    public boolean verifyPrivacyField(){
         privacy = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[4]"));
-        return privacy.getAttribute("class");
+        return privacy.isEnabled();
     }
-    public String verifyPublicPostsField(){
+    public boolean verifyPublicPostsField(){
         publicPosts = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[6]"));
-        return publicPosts.getAttribute("class");
+        return publicPosts.isEnabled();
     }
-    public String verifyBlockingField(){
+    public boolean verifyBlockingField(){
         blocking = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[7]"));
-        return blocking.getAttribute("class");
+        return blocking.isEnabled();
     }
-    public String verifyLocationField(){
+    public boolean verifyLocationField(){
         location = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[8]"));
-        return location.getTagName();
+        return location.isEnabled();
     }
-    public String verifyLanguageAndRegionField(){
+    public boolean verifyLanguageAndRegionField(){
         languageAndregion = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[9]"));
-        return languageAndregion.getAttribute("class");
+        return languageAndregion.isEnabled();
     }
-    public String verifyStoriesField(){
+    public boolean verifyStoriesField(){
         stories = (WebElement) driver.findElements(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[10]"));
-        return stories.getAttribute("class");
+        return stories.isEnabled();
     }
-    public String verifyBusinessIntegrationField(){
+    public boolean verifyBusinessIntegrationField(){
         businessIntegration = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 szxhu1pg hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6 ztn2w49o')])[16]"));
-        return businessIntegration.getAttribute("class");
+        return businessIntegration.isEnabled();
     }
 
 }
