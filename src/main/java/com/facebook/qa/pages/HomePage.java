d810fa973b5f40fd4ba0ee3e00b6742405b9e63f
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-12 00:51:38 -0700

    Update HomePage.java

--- src/main/java/com/facebook/qa/pages/HomePage.java
+++ src/main/java/com/facebook/qa/pages/HomePage.java
@@ -10,53 +10,67 @@ public class HomePage extends BaseClass {
     public static WebElement menu;
     public static WebElement friends;
     public static WebElement groups;
-    public static WebElement shortCutLogo;
-    public static WebElement shortcutField;
+    public static WebElement createNewGroup;
+    public static WebElement plusIcon;
     public static WebElement messenger;
-    public static WebElement iconOfPlus;
+    public static WebElement iconOfShareMoments;
     public static WebElement findFriendsLink;
+    public static WebElement addPictures;
 
-
-
-    public String verifySearchFieldOfHomePage() {
-        search = driver.findElement(By.xpath("//input[contains(@class,'qi72231t s3jn8y49 k14qyeqv mz1h5j5e ztn2w49o s19c0p35 febi1ev9 pccvoycu c84hr6m5 losq46hw qgrdou9d nu7423ey n3hqoq4p r86q59rh b3qcqh3k fq87ekyn f92fqwvt cavyzvzu bbgqxxx1 j4vk3mq5 bdao358l pbevjfx6 tgm57n0e cgu29s5g i15ihif8 no6h3tfh k1z55t6l aeinzg81 icdlwmnq om3e55n1 rh5b4hnh gy87pps4 b0eko5f3 r9fxt90l nq2b4knc jrp0dh92 m8m1d84o him0ws1g s0u2i99n oe2youzz mizpstq0 adj1mh9s h7nzrzxv gl9yfm1p ap9gnrge k4z0jo6w')]"));
-        return search.getAttribute("class");
+    public boolean verifySearchFieldOfHomePage() {
+        search = driver.findElement(By.xpath("(//input[@class='qi72231t s3jn8y49 k14qyeqv mz1h5j5e ztn2w49o s19c0p35 febi1ev9 pccvoycu c84hr6m5 losq46hw qgrdou9d nu7423ey n3hqoq4p r86q59rh b3qcqh3k fq87ekyn f92fqwvt cavyzvzu bbgqxxx1 j4vk3mq5 bdao358l pbevjfx6 tgm57n0e cgu29s5g i15ihif8 no6h3tfh k1z55t6l aeinzg81 icdlwmnq om3e55n1 rh5b4hnh gy87pps4 b0eko5f3 r9fxt90l fwlpnqze nq2b4knc adj1mh9s h7nzrzxv gl9yfm1p ap9gnrge k4z0jo6w'])[1]"));
+        return search.isDisplayed();
     }
-    public String verifyMenuFieldLogo() {
+    public boolean verifyMenuFieldLogo() {
         logo = driver.findElement(By.xpath("(//div[@class='qmqpeqxj e7u6y3za qwcclf47 nmlomj2f i85zmo3j frfouenu bonavkto djs4p424 r7bn319e bdao358l fxk3tzhb jcxyg2ei om3e55n1 a5wdgl2o h7q9eq3y iryl7rsq'])[1]"));
-        return logo.getAttribute("class");
+        return logo.isDisplayed();
     }
-    public String verifyMenuField() {
+    public MenuPage verifyMenuField() {
         menu = driver.findElement(By.xpath("(//span[@class='b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken tr46kb4q'])"));
-        return menu.getAttribute("class");
+        if(menu.isEnabled()){
+            menu.click();
+        }
+        return new MenuPage();
     }
-    public String verifyFriendsField() {
-        friends = driver.findElement(By.xpath("(//span)[5]"));
-        return friends.getAttribute("class");
+    public boolean verifyFriendsField() {
+        friends = driver.findElement(By.xpath("(//span[contains(@class,'b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken')])[5]"));
+        WebElement friendReq = driver.findElement(By.xpath("(//span[contains(@class,'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw hpj0pwwo sggt6rq5 tpi2lg9u pbevjfx6')])[2]"));
+        if(friends.isEnabled()){
+            friends.click();
+        }
+        return friendReq.isDisplayed();
     }
-    public String verifyGroupsField() {
+    public boolean verifyGroupsField() {
         groups = driver.findElement(By.xpath("(//span[@class='b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken tr46kb4q'])[2]"));
-        return groups.getAttribute("class");
+        WebElement discoverField = driver.findElement(By.xpath("(//span[@class='b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken'])[2]"));
+        if(groups.isEnabled()){
+            groups.click();
+        }
+        return discoverField.isEnabled();
     }
-    public String verifyShorcutFieldField() {
-        shortCutLogo = driver.findElement(By.xpath("(//i[@class='gneimcpu b0w474w7'])[3]"));
-        return shortCutLogo.getAttribute("class");
+    public boolean verifyCreateNewGroupField() {
+        createNewGroup = driver.findElement(By.xpath("(//span[@class='gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o'])"));
+        return createNewGroup.isEnabled();
     }
-    public String verifyShortcutField() {
-        shortcutField = driver.findElement(By.xpath("(//span[@class='b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken tr46kb4q'])[3]"));
-        return shortcutField.getAttribute("class");
+    public boolean verifyCreateNewGroupPlusIcon() {
+        plusIcon = driver.findElement(By.xpath("(//span[@class='b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken tr46kb4q'])[3]"));
+        return plusIcon.isDisplayed();
     }
-    public String verifymessengerField() {
-        messenger = driver.findElement(By.xpath("(//span[@class='b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken tr46kb4q'])[8]"));
-        return messenger.getAttribute("class");
+    public String verifyMessengerField() {
+        messenger = driver.findElement(By.xpath("(//div[@class='qi72231t n3hqoq4p r86q59rh b3qcqh3k fq87ekyn fsf7x5fv s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk cr00lzj9 rn8ck1ys s3jn8y49 f14ij5to l3ldwz01 icdlwmnq i85zmo3j qmqpeqxj e7u6y3za qwcclf47 nmlomj2f frfouenu bonavkto djs4p424 r7bn319e bdao358l alzwoclg jcxyg2ei srn514ro oxkhqvkx rl78xhln nch0832m om3e55n1 nq2b4knc bis24fgy a5wdgl2o'])"));
+        WebElement searchOnMessenger = driver.findElement(By.xpath("(//input[@class='qi72231t s3jn8y49 k14qyeqv mz1h5j5e ztn2w49o s19c0p35 febi1ev9 pccvoycu c84hr6m5 losq46hw qgrdou9d nu7423ey n3hqoq4p r86q59rh b3qcqh3k fq87ekyn f92fqwvt cavyzvzu bbgqxxx1 j4vk3mq5 bdao358l pbevjfx6 tgm57n0e cgu29s5g i15ihif8 no6h3tfh k1z55t6l aeinzg81 icdlwmnq om3e55n1 rh5b4hnh gy87pps4 i5oewl5a r9fxt90l nnzkd6d7 jvc6uz2b adj1mh9s h7nzrzxv gl9yfm1p ap9gnrge k4z0jo6w'])"));
+        if(messenger.isEnabled()){
+            messenger.click();
+            searchOnMessenger.sendKeys("rahul");
+        }
+        return searchOnMessenger.getAttribute("value");
     }
-    public String verifyPlusIconField() {
-        iconOfPlus = driver.findElement(By.xpath("(//i[@class='gneimcpu b0w474w7'])[3]"));
-        return iconOfPlus.getAttribute("class");
+    public boolean verifyShareMomentsIconField() {
+        iconOfShareMoments = driver.findElement(By.xpath("(//i[@class='gneimcpu b0w474w7'])[3]"));
+        return iconOfShareMoments.isEnabled();
     }
-    public String verifyFindFriendsLink() {
-        findFriendsLink = driver.findElement(By.xpath("(//span[@class='b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken'])[13]"));
-        return findFriendsLink.getAttribute("class");
+    public boolean verifyNotificatonsField() {
+        addPictures = driver.findElement(By.xpath("(//span)[58]"));
+        return addPictures.isEnabled();
     }
-
 }
