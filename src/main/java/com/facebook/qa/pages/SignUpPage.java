bd1a07e1c4372b658d043e48b544c417f42c79c4
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-12 00:55:41 -0700

    Update SignUpPage.java

--- src/main/java/com/facebook/qa/pages/SignUpPage.java
+++ src/main/java/com/facebook/qa/pages/SignUpPage.java
@@ -24,42 +24,66 @@ public class SignUpPage extends BaseClass {
     }
     public String verifyFirstNameField(){
         firstName = driver.findElement(By.xpath("(//input[@type='text'])[1]"));
-        return firstName.getAttribute("name");
+        if(firstName.isEnabled()){
+            firstName.sendKeys("Swapnil");
+        }
+        return firstName.getAttribute("value");
     }
     public String verifyLastNameField(){
         lastName = driver.findElement(By.name("lastname"));
-        return lastName.getAttribute("name");
+        if(lastName.isEnabled()){
+            lastName.sendKeys("Pawar");
+        }
+        return lastName.getAttribute("value");
     }
     public String verifyEmailTextField(){
         email = driver.findElement(By.name("reg_email__"));
-        return email.getAttribute("name");
+        if(email.isEnabled()){
+            email.sendKeys("swapnilPawar25@gmail.com");
+        }
+        return email.getAttribute("value");
     }
     public String verifyConfirmEmailTextField(){
         confirmEmail = driver.findElement(By.name("reg_email_confirmation__"));
-        return confirmEmail.getAttribute("name");
+        if(confirmEmail.isEnabled()){
+            confirmEmail.sendKeys("swapnilPawar25@gmail.com");
+        }
+        return confirmEmail.getAttribute("value");
     }
     public String verifyPasswordTextField(){
         password = driver.findElement(By.id("password_step_input"));
-        return password.getAttribute("name");
+        if(password.isEnabled()){
+            password.sendKeys("Swappy@2524");
+        }
+        return password.getAttribute("value");
     }
     public String verifyBirthDayField(){
         birthDay = driver.findElement(By.name("birthday_day"));
-        return birthDay.getAttribute("name");
+        if(birthDay.isEnabled()){
+            birthDay.sendKeys("5");
+        }
+        return birthDay.getAttribute("value");
     }
     public String verifyBirthmonthField(){
         birthmonth = driver.findElement(By.name("birthday_month"));
-        return birthmonth.getAttribute("name");
+        if(birthmonth.isEnabled()){
+            birthmonth.sendKeys("Sep");
+        }
+        return birthmonth.getAttribute("value");
     }
     public String verifyBirthYearField(){
         birthYear = driver.findElement(By.name("birthday_year"));
-        return birthYear.getAttribute("name");
+        if(birthYear.isEnabled()){
+            birthYear.sendKeys("1999");
+        }
+        return birthYear.getAttribute("value");
     }
     public String verifyGenderField(){
         gender = driver.findElements(By.name("sex")).get(1);
-        return gender.getAttribute("name");
+        return gender.getAttribute("value");
     }
-    public String verifySignUpBtnField(){
+    public boolean verifySignUpBtnField(){
         submitBtn = driver.findElement(By.name("websubmit"));
-        return submitBtn.getAttribute("name");
+        return submitBtn.isEnabled();
     }
 }
