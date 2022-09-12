78c813255306d3ad083a48ef070919e926ed00d8
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-12 00:57:26 -0700

    Update LogInPageTestCase.java

--- src/test/java/com/facebook/qa/testcasses/LogInPageTestCase.java
+++ src/test/java/com/facebook/qa/testcasses/LogInPageTestCase.java
@@ -1,6 +1,8 @@
 package com.facebook.qa.testcasses;
 
+import com.facebook.qa.pages.HomePage;
 import com.facebook.qa.pages.LogInPage;
+import com.facebook.qa.pages.SignUpPage;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -21,51 +23,50 @@ public class LogInPageTestCase extends LogInPage {
         Assert.assertEquals(title,"Facebook – log in or sign up");
     }
     @Test(priority = 2)
-    public void verifyIdTextField(){
-        String userName = verifyUserId1TextField();
-        Assert.assertEquals(userName,"email");
+    public void verifyId2TextField(){
+        boolean userName2 = verifyUserId2TextField();
+        Assert.assertTrue(userName2);
     }
     @Test(priority = 3)
-    public void verifyPasswordOfLogInPage(){
-        String passLogIn1 = verifyPass1TextFieldOfLogInPage();
-        Assert.assertEquals(passLogIn1,"pass");
+    public void verifyIdTextField(){
+        String userName = verifyUserId1TextField();
+        Assert.assertEquals(userName,"sopiakshu25@gmail.com");
     }
     @Test(priority = 4)
-    public void verifyBtnOfLogInPage1(){
-        String button1 = verifyLogInBtn1();
-        Assert.assertEquals(button1,"login");
+    public void verifyPasswordOfLogInPage(){
+        String passLogIn1 = verifyPass1TextFieldOfLogInPage();
+        Assert.assertEquals(passLogIn1,"Sopi@1234");
     }
     @Test(priority = 5)
-    public void verifyForgottenLinkOfPage(){
-        String forgotLink = verifyForgotPassLink();
-        Assert.assertEquals(forgotLink,"_6ltj");
-    }
-    @Test(priority = 6)
-    public void verifyCreateAccBtnOfPage(){
-        String createBtn = verifyCreateAccountBtn();
-        Assert.assertEquals(createBtn,"_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy");
-    }
-    @Test(priority = 7)
-    public void verifyId2TextField(){
-        String userName2 = verifyUserId2TextField();
-        Assert.assertEquals(userName2,"email");
-    }
-    @Test(priority = 8)
     public void verifyPasswordOfLogInPage2(){
-        String passLogIn2 = verifyPass2TextFieldOfLogInPage();
+        boolean passLogIn2 = verifyPass2TextFieldOfLogInPage();
         Assert.assertEquals(passLogIn2,"pass");
     }
-    @Test(priority = 9)
-    public void verifyCreateAccBtnOfPageIsEnabledOrNot(){
-        boolean createBtn1 = verifyCreateAccountBtnIsEnabled();
-        Assert.assertTrue(createBtn1);
+    @Test(priority = 6)
+    public void verifyBtnOfLogInPage1(){
+        boolean button1 = verifyLogInBtn1();
+        Assert.assertTrue(button1);
     }
-    @Test(priority = 10)
+    @Test(priority = 7)
     public void verifyBtnOfLogInPage2(){
         boolean button2 = verifyLogInBtn2();
         Assert.assertTrue(button2);
     }
-     @Test(priority = 11)
+    @Test(priority = 8)
+    public void verifyForgottenLinkOfPage(){
+        String forgotLink = verifyForgotPassLink();
+        Assert.assertEquals(forgotLink,"https://www.facebook.com/#");
+    }
+    @Test(priority = 9)
+    public void verifyCreateAccBtnEnabledOrNotOfPage(){
+        boolean Btn = verifyCreateAccountBtnEnabled();
+        Assert.assertTrue(Btn);
+    }
+    @Test(priority = 10)
+    public void verifyCreateAccBtnOfPage(){
+        SignUpPage clickOnBtn = verifyCreateAccountBtn();
+    }
+    @Test(priority = 11)
     public void verifyLogInField(){
        HomePage homepage = verifyLogInField("sopiakshu25@gmail.com", "Sopi@1234");
     }
