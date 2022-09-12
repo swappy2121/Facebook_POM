ca9231ccfa4f5b0e4e16a7f2c706acbe2b1f1475
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-11 21:23:39 -0700

    Update ProfilePageTestCase.java

--- src/test/java/com/facebook/qa/testcasses/ProfilePageTestCase.java
+++ src/test/java/com/facebook/qa/testcasses/ProfilePageTestCase.java
@@ -18,7 +18,7 @@ public class ProfilePageTestCase extends ProfilePage {
         driver.findElement(By.name("email")).sendKeys("sopiakshu25@gmail.com");
         driver.findElement(By.name("pass")).sendKeys("Sopi@1234");
         driver.findElement(By.name("login")).click();
-        driver.findElement(By.xpath("(//span[contains(@class,'b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken')])[2]")).click();
+        driver.get("https://www.facebook.com/profile.php?id=100085138144054");
     }
     @Test(priority = 1)
     public void verifyPostsFieldForProfilePage(){
@@ -28,50 +28,50 @@ public class ProfilePageTestCase extends ProfilePage {
     @Test(priority = 2)
     public void verifyAboutFieldLogoInProfilePage(){
         String aboutField  = verifyAboutField();
-        Assert.assertEquals(aboutField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w innypi6y rtxb060y");
+        Assert.assertEquals(aboutField,"https://www.facebook.com/profile.php?id=100085138144054");
     }
     @Test(priority = 3)
-    public void verifyMoreFieldForProfilePage(){
-        String moreField = verifyMoreField();
-        Assert.assertEquals(moreField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w innypi6y rtxb060y");
+    public void verifySearchFieldForProfilePage(){
+        boolean searchField = verifySearchField();
+        Assert.assertTrue(searchField);
     }
     @Test(priority = 4)
     public void verifyFriendsFieldOfProfilePage(){
         String friendField = verifyFriendsField();
-        Assert.assertEquals(friendField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w innypi6y rtxb060y");
+        Assert.assertEquals(friendField,"Akshay Pawar");
     }
     @Test(priority = 5)
     public void verifyPhotosFieldOfPage(){
-        String photosField = verifyPhotosField();
-        Assert.assertEquals(photosField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w innypi6y rtxb060y");
+        boolean photosField = verifyPhotosField();
+        Assert.assertTrue(photosField);
     }
     @Test(priority = 6)
-    public void verifyvideosFieldOfPage(){
-        String videosField = verifyVideosField();
-        Assert.assertEquals(videosField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w innypi6y rtxb060y");
+    public void verifyvideosFieldOfPage() {
+        boolean videosField = verifyVideosField();
+        Assert.assertTrue(videosField);
     }
     @Test(priority = 7)
     public void verifyCheck_InFieldOfPage(){
-        String checkInField = verifyCheck_InField();
-        Assert.assertEquals(checkInField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w innypi6y rtxb060y");
+        boolean checkInField = verifyCheck_InField();
+        Assert.assertTrue(checkInField);
     }
     @Test(priority = 8)
-    public void verifyAddBioFieldOfPage(){
-        String addBioField = verifyAddBioField();
-        Assert.assertEquals(addBioField,"b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken");
+    public void verifyAddStoryFieldOfPage(){
+        boolean addStoryField = verifyAddStoryField();
+        Assert.assertTrue(addStoryField);
     }
     @Test(priority = 9)
     public void verifyEditDetailsFieldOfProfilePage(){
-        String editDetailsOfField = verifyEditDetailsField();
-        Assert.assertEquals(editDetailsOfField,"b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken");
+        boolean editProfileDetails = verifyEditProfileDetailsField();
+        Assert.assertTrue(editProfileDetails);
     }
     @Test(priority = 10)
-    public void verifyAddHobbiesFieldOfHomePage(){
-        String addHobbiesField = verifyAddHobbiesDetailsField();
-        Assert.assertEquals(addHobbiesField,"b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken");
+    public void verifyAddCoverPhotosFieldOfProfilePage(){
+        boolean addCoverPhotos = verifyAddCoverPhotosField();
+        Assert.assertTrue(addCoverPhotos);
     }
     @AfterTest
     public void closeBrowsers(){
-          driver.quit();
+        driver.quit();
     }
 }
