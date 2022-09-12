31117e8ffd8c1528ab2777056b75f2f644d69a1b
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-09-12 00:58:01 -0700

    Update MenuPageTestCase.java

--- src/test/java/com/facebook/qa/testcasses/MenuPageTestCase.java
+++ src/test/java/com/facebook/qa/testcasses/MenuPageTestCase.java
@@ -19,7 +19,7 @@ public class MenuPageTestCase extends MenuPage {
         driver.findElement(By.name("email")).sendKeys("sopiakshu25@gmail.com");
         driver.findElement(By.name("pass")).sendKeys("Sopi@1234");
         driver.findElement(By.name("login")).click();
-        driver.findElement(By.xpath("(//span[contains(@class,'b6ax4al1 lq84ybu9 hf30pyar om3e55n1 oshhggmv qm54mken')])[2]")).click();
+        driver.findElement(By.xpath("//div[contains(@class,'qi72231t nu7423ey tav9wjvu flwp5yud tghlliq5 gkg15gwv s9ok87oh s9ljgwtm lxqftegz bf1zulr9 frfouenu bonavkto djs4p424 r7bn319e bdao358l fsf7x5fv tgm57n0e jez8cy9q s5oniofx l7ghb35v kmwttqpk dnr7xe2t aeinzg81 om3e55n1 cr00lzj9 rn8ck1ys s3jn8y49 g4tp4svg o9erhkwx dzqi5evh hupbnkgi hvb2xoa8 jl2a5g8c f14ij5to l3ldwz01 icdlwmnq i85zmo3j alzwoclg irrdun15 hgxaigo5 d2hqwtrz o9wcebwi fzsidkae l3gxykhd')]")).click();
     }
     @Test(priority = 1)
     public void verifyFB_LogoForMenuPage(){
@@ -28,48 +28,48 @@ public class MenuPageTestCase extends MenuPage {
     }
     @Test(priority = 2)
     public void verifyWelcomeFieldLogoInMenuPage(){
-        String welcomeField  = verifyWelcomeField();
-        Assert.assertEquals(welcomeField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+        boolean welcomeField  = verifyWelcomeField();
+        Assert.assertTrue(welcomeField);
     }
     @Test(priority = 3)
     public void verifyEventFieldForMenuPage(){
         String eventField = verifyEventField();
-        Assert.assertEquals(eventField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+        Assert.assertEquals(eventField,"funny videos");
     }
     @Test(priority = 4)
     public void verifyFindFriendsFieldOfMenuPage(){
-        String FindFriendField = verifyFindFriendsField();
-        Assert.assertEquals(FindFriendField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+        boolean findFriendField = verifyFindFriendsField();
+        Assert.assertTrue(findFriendField);
     }
     @Test(priority = 5)
-    public void verifyGroupsFieldOfPage(){
-        String groups = verifyGroupsField();
-        Assert.assertEquals(groups,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+    public void verifyGroupsFieldOfPage() {
+        boolean groups = verifyGroupsField();
+        Assert.assertTrue(groups);
     }
     @Test(priority = 6)
-    public void verifyNewsFieldOfPage(){
-        String newsField = verifyNewsField();
-        Assert.assertEquals(newsField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+    public void verifyNewsFieldOfPage() {
+        boolean newsField = verifyNewsField();
+        Assert.assertTrue(newsField);
     }
     @Test(priority = 7)
-    public void verifyFavouritesFieldOfPage(){
-        String favouritesField = verifyFavouritesField();
-        Assert.assertEquals(favouritesField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+    public void verifyFavouritesFieldOfPage() {
+        boolean favouritesField = verifyFavouritesField();
+        Assert.assertTrue(favouritesField);
     }
     @Test(priority = 8)
-    public void verifyWatchVideosFieldOfPage(){
-        String watchVideosField = verifyWatchVideosField();
-        Assert.assertEquals(watchVideosField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+    public void verifyWatchVideosFieldOfPage() {
+        boolean watchVideosField = verifyWatchVideosField();
+        Assert.assertTrue(watchVideosField);
     }
     @Test(priority = 9)
-    public void verifyGamingFieldOfMenuPage(){
-        String gamingField = verifyGamingField();
-        Assert.assertEquals(gamingField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+    public void verifyGamingFieldOfMenuPage() {
+        boolean gamingField = verifyGamingField();
+        Assert.assertTrue(gamingField);
     }
     @Test(priority = 10)
     public void verifyBloodDonationFieldOfMenuPage(){
-        String bloodDonationField = verifyBloodDonationField();
-        Assert.assertEquals(bloodDonationField,"gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tpi2lg9u pbevjfx6 ztn2w49o");
+        boolean bloodDonationField = verifyBloodDonationField();
+        Assert.assertTrue(bloodDonationField);
     }
     @AfterTest
     public void closeBrowsers(){
